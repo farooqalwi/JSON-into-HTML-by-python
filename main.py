a4e92c1d24a6f7e1d7cd57cedd5e64f0266cab45
Farooq Alwi <farooqalwi313@gmail.com>
2021-09-29 09:08:19 +0500

    main function split into smaller functions

--- main.py
+++ main.py
@@ -8,52 +8,68 @@ __version__ = "0.1.0"
 __license__ = "MIT"
 
 import argparse
-from genericpath import exists, isdir, isfile
-from os import remove
 from logzero import logger, logfile
 import json
 import sys
 import os
 from datetime import datetime
-now = datetime.now()
 
 
-def main(args):
-    """ Main entry point of the app """
-    path = args.arg
-
+def validatePath_folder(path):
+    """this function validates user given folder path, 
+    incase valid path returns the path"""
     if(os.path.isdir(path)):
-        filePath = f"{os.path.join(path, 'result.json')}"
-        if(os.path.isfile(filePath)):
-            with open(filePath, "r", encoding='utf-8') as f:
-                try:
-                    json_file = json.load(f)
-                except json.decoder.JSONDecodeError as err:
-                    logger.error(f"Invalid JSON provided <{err}>")
-                    sys.exit(1)
-        else:
-            logger.error(f"<{path}> does not contain 'result.json' file")
-            sys.exit(1)
+        return path
     else:
         logger.error("Path is not a folder")
         sys.exit(1)
+
+def isExist_JSON(folderPath):
+    """it checks either 'json' file exists or not"""
+    filePath = f"{os.path.join(folderPath, 'result.json')}"
+    if(os.path.isfile(filePath)):
+        return filePath
+    else:
+        logger.error(f"<{folderPath}> does not contain 'result.json' file")
+        sys.exit(1)
+
+def readJson(filePath):
+    """it reads json data, if valid json returns 
+    the data otherwise exit the program"""
+    with open(filePath, "r", encoding='utf-8') as f:
+        try:
+            json_file = json.load(f)
+        except json.decoder.JSONDecodeError as err:
+            logger.error(f"Invalid JSON provided <{err}>")
+            sys.exit(1)
+        return json_file
     
-    # for output file name, default file name is current timestamp from step 60 "output-2021-09-26 0110.html"
-    dt_string = now.strftime("%Y-%m-%d %H%M")
-    defaultFileName = f"result-{dt_string}.html"
-    outputPath = f"{path}\\{defaultFileName}"
-
-    # channel content
-    channelName = json_file["name"]
-    channelType = json_file["type"]
-    messages = json_file["messages"]
-
-    # for sorting order, 'asc' for ascending, 'desc' for descending. by default sorted by 'desc'
+def sortData(messages):
+    """it sorts the json data, 'asc' for ascending, 
+    'desc' for descending. by default sorted by 'desc'"""
     if(args.sort == "desc"):
         messages.reverse()
     elif(args.sort != "asc"):
         logger.error("sort order mismatch")
         sys.exit(1)
+
+def createHTML(folderPath, json_data):
+    """it creates the html file from json data"""
+
+    # for output file name, default file name is current 
+    # timestamp from step 60 "output-2021-09-26 0110.html"
+    now = datetime.now()
+    dt_string = now.strftime("%Y-%m-%d %H%M")
+    defaultFileName = f"result-{dt_string}.html"
+    outputPath = os.path.join(folderPath, defaultFileName)
+
+    # channel content
+    channelName = json_data["name"]
+    channelType = json_data["type"]
+    messages = json_data["messages"]
+
+    # to sort order od data
+    sortData(messages)
 
     try:
         with open(outputPath, 'w', encoding='utf-8') as f:
@@ -81,15 +97,15 @@ def main(args):
                 if("photo" in message.keys()):
                     photo = message["photo"]
                     photoDir = photo[0:7]
-                    if(os.path.isdir(f"{path}\\{photoDir}")):
-                        if(os.path.isfile(f"{path}\\{photo}")):
+                    if(os.path.isdir(os.path.join(folderPath, photoDir))):
+                        if(os.path.isfile(os.path.join(folderPath, photo))):
                             f.write(f"""
                                     <img src={photo} style="width: 260px; height: 251px"/>
                                     """)
                         else:
                             logger.error(f"<{photo}> image not found")
                     else:
-                        logger.error(f"<{path}\\{photoDir}> directory not found")
+                        logger.error(f"<{os.path.join(folderPath, photoDir)}> directory not found")
                 else:
                     logger.error(f"image not found at Post ID: {postID}")
                 
@@ -133,7 +149,6 @@ def main(args):
                         f.write(f"""
                                     <p>{wholeText}</p>
                                 """)
-    
             f.write("""
                         </body>
                     </html>
@@ -143,15 +158,38 @@ def main(args):
         sys.exit(1)
     
     logger.info("HTML file created")
+
+def createLog():
+    """it creates log file, default file name is current timestamp 
+    "log-20210928220001.log" year month date hour minute seconds"""
+    now = datetime.now()
+    dt_string = now.strftime("%Y%m%d%H%M%S")
+    logfile(os.path.join("logs", f"log-{dt_string}.log"))
+
+def main(args):
+    """ Main entry point of the app """
+
+    # path from cmd
+    path = args.arg
+
+    # folder path after validation
+    folderPath = validatePath_folder(path)
+
+    # json file path
+    jsonPath = isExist_JSON(folderPath)
+
+    # json data
+    json_data = readJson(jsonPath)
+
+    # html creating
+    createHTML(folderPath, json_data)
+    
     sys.exit(0)
 
 
 if __name__ == "__main__":
-    """ This is executed when run from the command line """
-    # for log file name, default file name is current timestamp "log-20210928220001.log" year month date hour minute seconds
-    dt_string = now.strftime("%Y%m%d%H%M%S")
-    logfile(os.path.join("logs", f"log-{dt_string}.log"))
-    
+    """ This is executed when run from the command line """ 
+    createLog()
     parser = argparse.ArgumentParser()
 
     # Required positional argument
