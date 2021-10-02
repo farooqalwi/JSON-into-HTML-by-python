56b09dbb05015b7c2fa0a0f102ecf5b9299aa342
Farooq Alwi <farooqalwi313@gmail.com>
2021-10-02 20:57:55 +0500

    step 180 done

--- main.py
+++ main.py
@@ -68,24 +68,6 @@ def sort_data(args, messages):
         raise FunctionFailed from Exception
 
 
-def write_basic_chennal_info(json_data):
-    """it writes the chennal basic info,
-    i.e: chenal name, chennal type, chennal link"""
-    channel_name = json_data["name"]
-    channel_type = json_data["type"]
-    return f"""
-                <!DOCTYPE html>
-                <html>
-                <head>
-                    <title>JSON to HTML</title>
-                </head>
-                <body>
-                <h1>{channel_name}</h1>
-                <a href="https://t.me/themuslimarchive">https://t.me/themuslimarchive</a>
-                <p>{channel_type}</p>
-            """
-
-
 def output_file(folderpath):
     """for output file name, default file name is current
     timestamp from step 60 'output-2021-09-26 0110.html'"""
@@ -96,142 +78,22 @@ def output_file(folderpath):
     return output_path
 
 
-def validate_image_html(folderpath, message, post_id):
-    """it valliadtes images either present or not"""
-    photo_existed = ""
-    if "photo" in message.keys():
-        photo = message["photo"]
-        photo_dir = photo[0:7]
-        if os.path.isdir(os.path.join(folderpath, photo_dir)):
-            if os.path.isfile(os.path.join(folderpath, photo)):
-                photo_existed = photo
-            else:
-                logger.error("<%s> image not found", photo)
-        else:
-            logger.error(
-                "<%s> directory not found", os.path.join(folderpath, photo_dir)
-            )
-            raise FunctionFailed from Exception
-
-    else:
-        logger.error("image not found at Post ID: %s", post_id)
-    return photo_existed
-
-
-def text_formatting(list_text):
-    """it checks bold, code, italic, underline, strikethrough type"""
-    if list_text["type"] == "bold":
-        whole_text = f"""<strong>{list_text["text"]}</strong>"""
-    elif list_text["type"] == "code":
-        whole_text = f"""<code>{list_text["text"]}</code>"""
-    elif list_text["type"] == "italic":
-        whole_text = f"""<em>{list_text["text"]}</em>"""
-    elif list_text["type"] == "underline":
-        whole_text = f"""<u>{list_text["text"]}</u>"""
-    elif list_text["type"] == "strikethrough":
-        whole_text = f"""<s>{list_text["text"]}</s>"""
-
-    return whole_text
-
-
-def write_text_html(text):
-    """it writes texts with respective formatting"""
-    whole_text = ""
-    for list_text in text:
-        if list_text != " ":
-            if isinstance(list_text, str):
-                whole_text += list_text
-            elif isinstance(list_text, dict):
-                if (
-                    list_text["type"] == "bold"
-                    or list_text["type"] == "code"
-                    or list_text["type"] == "italic"
-                    or list_text["type"] == "underline"
-                    or list_text["type"] == "strikethrough"
-                ):
-                    whole_text += text_formatting(list_text)
-                elif list_text["type"] == "link":
-                    whole_text += (
-                        f"""<a href="{list_text["text"]}">{list_text["text"]}</a>"""
-                    )
-                elif list_text["type"] == "text_link":
-                    whole_text += (
-                        f"""<a href="{list_text["href"]}">{list_text["text"]}</a>"""
-                    )
-                elif list_text["type"] == "hashtag":
-                    whole_text += f"""<a href="#" style="text-decoration:none">
-                                    {list_text["text"]} </a>"""
-                elif list_text["type"] == "mention":
-                    whole_text += f"""<a href="https://t.me/
-                                    {list_text["text"][1:]}">{list_text["text"]}</a>"""
-                elif list_text["type"] == "email":
-                    whole_text += f"""<a href="mailto:
-                                    {list_text["text"]}">{list_text["text"]}</a>"""
-                elif list_text["type"] == "phone":
-                    whole_text += (
-                        f"""<a href="tel:{list_text["text"]}">{list_text["text"]}</a>"""
-                    )
-    whole_text = whole_text.replace("\n", "<br>")
-    return whole_text
-
-
-def create_html(args, folderpath, json_data):
-    """it creates the html file from json data"""
+def create_html_with_jinja(args, folderpath, json_data):
+    """it creates the html file from json data by using jinja"""
     messages = json_data["messages"]
     # to sort order od data
     sort_data(args, messages)
-    with open(output_file(folderpath), "w", encoding="utf-8") as file:
-        file.write(write_basic_chennal_info(json_data))
-        for message in messages:
-            post_id = message["id"]
-            post_date = message["date"]
-            file.write(
-                f"""
-                    <hr>
-                    <h2>Date: {post_date}</h2>
-                    <p>id: {post_id}</p>
-                    """
-            )
-            photo = validate_image_html(folderpath, message, post_id)
-            if photo != "":
-                file.write(
-                    f"""
-                        <img src={photo} style="width: 260px; height: 251px"/>
-                        """
-                )
-            text = message["text"]
-            if text != "":
-                if isinstance(text, str):
-                    file.write(
-                        f"""
-                                <p>{text}</p>
-                            """
-                    )
-                elif isinstance(text, list):
-                    whole_text = write_text_html(text)
-                    file.write(
-                        f"""
-                                <p>{whole_text}</p>
-                            """
-                    )
-        file.write(
-            """
-                    </body>
-                </html>
-                """
-        )
-    logger.info("HTML file created")
-
-
-def create_html_with_jinja(json_data):
-    """it creates the html file from json data by using jinja"""
     file_loader = FileSystemLoader("templates")
     env = Environment(loader=file_loader)
-    rendered = env.get_template("template.html").render(content=json_data)
+    rendered = env.get_template("template.html").render(
+        content=json_data, messages=messages
+    )
 
     # write html to a file - index.html in root folder
-    with open("index.html", "w", encoding="utf-8") as file:
+    with open(output_file(folderpath), "w", encoding="utf-8") as file:
         file.write(rendered)
+
+    logger.info("HTML file created")
 
 
 def create_log():
@@ -277,10 +139,8 @@ def main():
         json_path = is_exist_json(folderpath)
         # json data
         json_data = read_json(json_path)
-        # html creating
-        create_html(args, folderpath, json_data)
         # html creating by using jinja
-        create_html_with_jinja(json_data)
+        create_html_with_jinja(args, folderpath, json_data)
         sys.exit(0)
     except FunctionFailed:
         logger.error("Program terminated unspectedly")
